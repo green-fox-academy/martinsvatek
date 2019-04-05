c646af5fedfcee1101c1d06702dcd06242d8a2f6
martinsvatek <svatek.ma@gmail.com>
2019-04-05 12:04:15 +0200

    Solved trouble with Cuboid

--- week-1/day-4/3. Exercise/src/Cuboid.java
+++ week-1/day-4/3. Exercise/src/Cuboid.java
@@ -2,18 +2,19 @@
 import java.util.Scanner;
 
     public class Cuboid {
-        public static void main(double[] args) {
+        public static void main(String[] args) {
             Scanner myObj = new Scanner(System.in);
 
             System.out.println("Enter lenght");
-            double lenght = myObj.nextLine();
+            double lenght = myObj.nextDouble();
 
             System.out.println("Enter width");
-            double width = myObj.nextLine();
+            double width = myObj.nextDouble();
 
             System.out.println("Enter height");
-            double height = myObj.nextLine();
+            double height = myObj.nextDouble();
 
-            System.out.println("Volume: " lenght * width * height);
+            System.out.println("Volume: " + lenght * width * height);
+            System.out.println("Volume: " + lenght * width * height);
         }
     }

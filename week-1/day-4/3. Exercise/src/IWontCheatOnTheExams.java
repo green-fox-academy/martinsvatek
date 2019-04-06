a4af9c773460f1807e59c4cd015109867f1c6936
martinsvatek <svatek.ma@gmail.com>
2019-04-06 19:44:55 +0200

    how to write line 100 times

---
+++ week-1/day-4/3. Exercise/src/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+public class IWontCheatOnTheExams {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+
+    public static void main(String[] args) {
+
+        for (int a = 0; a < 100; a++) {
+            int b = a + 1;
+            System.out.println(b + ": I won't cheat on the exam!");
+        }
+    }
+}

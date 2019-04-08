858f192b18ac5c6017618559ecbaae434611a51e
martinsvatek <svatek.ma@gmail.com>
2019-04-08 16:36:09 +0200

    some others similar exercises

---
+++ week-2/day-1/4-Exercise-javaFunctionsAarrays/DiagonalMatrix.java
@@ -0,0 +1,34 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+
+import java.util.Scanner;
+
+public class DiagonalMatrix {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me length of the matrix side A: ");
+        int matrixSideA = scanner.nextInt();
+        System.out.println("Give me length of the matrix side B: ");
+        int matrixSideB = scanner.nextInt();
+
+        int[][] matrix = new int[matrixSideA][matrixSideB];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (j == i){
+                    matrix[i][j] = 1;
+                }
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}

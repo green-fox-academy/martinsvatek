e87488315d921f0d844cd3c9f78081c406bf03fd
martinsvatek <svatek.ma@gmail.com>
2019-04-15 16:17:01 +0200

    'WriteSingleLine' exercise added

--- week-3/day-1/WriteSingleLine.java
+++ week-3/day-1/WriteSingleLine.java
@@ -1,12 +1,35 @@
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class WriteSingleLine {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("HEY YOU, don't watch at me and type your name:");
+        String singleLine = scanner.nextLine();
+
+        writeToFile(singleLine);
+    }
+
+    public static void writeToFile(String singleLine) {
+
+        try {
+            Path filePath = Paths.get("src/my-file.txt");
+            List<String> content = new ArrayList<>();
+            content.add (singleLine);
+            Files.write(filePath, content);
+
+        } catch (IOException e) {
+
+            System.out.println("Unable to write file: my-file.txt");
+        }
     }
 }
 

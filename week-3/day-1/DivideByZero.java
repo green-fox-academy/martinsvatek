be2cfecc6c16f196b4847ea372a19e02bac39f4f
martinsvatek <svatek.ma@gmail.com>
2019-04-15 13:58:53 +0200

    'DivideByZero' exercise added

---
+++ week-3/day-1/DivideByZero.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DivideByZero {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("By which number do you want to divide number 10? Dont be afraid and tell me!!");
+        int number = scanner.nextInt();
+
+        tenDividedBy(number);
+    }
+
+    public static void tenDividedBy(int number) {
+
+        try {
+            double result = 10 / number;
+
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Fail! Dont be stupid and use different number!");
+        }
+    }
+}
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0

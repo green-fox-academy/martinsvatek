7013e3311a09d9883704b31897c24251d092f3b8
martinsvatek <svatek.ma@gmail.com>
2019-04-16 14:22:19 +0200

    'BlogPost' exercise added

--- week-3/day-2/PostIt/PostIt.java
+++ week-3/day-2/PostIt/PostIt.java
@@ -19,3 +19,12 @@ public class PostIt {
         System.out.println("text color is: " + this.textColor);
     }
 }
+
+//Create a PostIt class that has
+    // a backgroundColor
+    // a text on it
+    // a textColor
+// Create a few example post-it objects:
+    // an orange with blue text: "Idea 1"
+    // a pink with black text: "Awesome"
+    // a yellow with green text: "Superb!"

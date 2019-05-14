780b9c5fe10fa02a14c6d4ffad523ae42d4b2a48
martinsvatek <svatek.ma@gmail.com>
2019-05-14 13:57:09 +0200

    'Even Numbers' exercise added

---
+++ w6/d2/src/EvenNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+    }
+}
+
+// Write a Stream Expression to get the even numbers from the following list:

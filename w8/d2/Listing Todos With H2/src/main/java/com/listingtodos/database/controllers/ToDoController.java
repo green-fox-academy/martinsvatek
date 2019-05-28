5332cb2059487845f8e6016d4fc6c742479f7a1f
martinsvatek <svatek.ma@gmail.com>
2019-05-28 17:34:03 +0200

    End of a day

--- w8/d2/Listing Todos With H2/src/main/java/com/listingtodos/database/controllers/ToDoController.java
+++ w8/d2/Listing Todos With H2/src/main/java/com/listingtodos/database/controllers/ToDoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // Create a new controller called TodoController which maps to /todo
@@ -29,6 +30,8 @@ public class ToDoController {
         this.toDoRepo = toDoRepo;
         toDoRepo.save(new ToDo("Clean your room!", true, false));
         toDoRepo.save(new ToDo("Do homework!", false, false));
+        toDoRepo.save(new ToDo("Run!", true, false));
+        toDoRepo.save(new ToDo("Learn English yourself", false, false));
     }
 
     // Add a public String list() method which maps to / and /list in the controller, which returns with
@@ -40,6 +43,16 @@ public class ToDoController {
 
         // In the list method of the Controller use the repository to find all elements
         // and add them to the model as "todos" attribute
+
+        model.addAttribute("todos", toDoRepo.findAll()); // when you ise "todos" in html, it gives you all from repo
+        return "todolist";
+    }
+
+    @GetMapping(value = {"/","/list"})
+    public String list(Model model, @RequestParam(value = "isDone", defaultValue = "false") boolean done) {
+        if (done) {
+            model.addAttribute("todos", toDoRepo.fi());
+        } else
 
         model.addAttribute("todos", toDoRepo.findAll()); // when you ise "todos" in html, it gives you all from repo
         return "todolist";

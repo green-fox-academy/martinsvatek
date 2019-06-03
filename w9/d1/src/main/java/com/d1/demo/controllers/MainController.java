c23f51832bc0f60549c8ffde051902755e20d51d
martinsvatek <svatek.ma@gmail.com>
2019-06-03 17:18:33 +0200

    'Greeter' added

--- w9/d1/src/main/java/com/d1/demo/controllers/MainController.java
+++ w9/d1/src/main/java/com/d1/demo/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.d1.demo.controllers;
 
 import com.d1.demo.models.Doubling;
+import com.d1.demo.models.Greeter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,5 +23,12 @@ public class MainController {
         } else {
             return new Doubling();
         }
+    }
+
+    @GetMapping (value = "/greeter")
+    @ResponseBody // we return text only, not template
+    public Object greeter(@RequestParam (value = "name", required = false) String name,
+                           @RequestParam (value = "title", required = false) String title) {
+        return new Greeter(name, title);
     }
 }
--- w9/d1/src/main/java/com/d1/demo/models/Greeter.java
+++ w9/d1/src/main/java/com/d1/demo/models/Greeter.java
@@ -1,4 +1,43 @@
 package com.d1.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public class Greeter {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String welcome_message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String error;
+
+
+
+    public Greeter(String name, String title) {
+
+        if (title == null && name == null) {
+            this.error = "Please provide a name and a title!";
+        } else if (name == null) {
+            this.error = "Please provide a name!";
+        } else if (title == null) {
+            this.error = "Please provide a title!";
+        } else {
+            this.welcome_message = "Oh, hi there " + name + ", my dear " + title + "!";
+        }
+    }
+
+    public String getWelcome_message() {
+        return welcome_message;
+    }
+
+    public void setWelcome_message(String welcome_message) {
+        this.welcome_message = welcome_message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
